4222bd33123b95bf2b4bb52eb09953e2047ea8d9
Alex Lam <amzfree@gmail.com>
2018-02-27 15:35:06 +0800

    update to lecture 156

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
-import { Card, CardSection, Input, Button } from './common';
+import { connect } from 'react-redux';
+import { employeeUpdate, employeeCreate } from '../actions';
+import { Card, CardSection, Button } from './common';
+import EmployeeForm from './EmployeeForm';
 
 class EmployeeCreate extends Component {
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+  }
   render() {
+    console.log(this.props);
     return (
       <Card>
+        <EmployeeForm {...this.props} />
         <CardSection>
-          <Input
-            label="Name"
-            placeholder="Jane"
-          />
-        </CardSection>
-        <CardSection>
-          <Input
-            label="Phone"
-            placeholder="23456789"
-          />
-        </CardSection>
-        <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Create
           </Button>
         </CardSection>
@@ -27,4 +24,11 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+
+
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+}
+
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,18 +1,33 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { FlatList } from 'react-native';
+import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeesFetch();
+  }
+  renderItem(employee) {
+    return <ListItem employee={employee} />;
+  }
   render() {
     return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
+      <FlatList
+        data={this.props.employees}
+        renderItem={this.renderItem}
+        keyExtractor={employee => employee.uid}
+      />
     );
   }
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
+  return { employees };
+}
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
